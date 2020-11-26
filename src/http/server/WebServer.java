2200c42b233e1fbab46fb7f372e857316ccca78f
nicolas-trouin <nicolas.trouin@insa-lyon.fr>
2020-11-26 09:44:51 +0100

    PUT :bug:

--- src/http/server/WebServer.java
+++ src/http/server/WebServer.java
@@ -8,6 +8,7 @@ import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 import static java.lang.System.exit;
 
@@ -38,7 +39,8 @@ public class WebServer {
 
                 String str = in.readLine();
                 System.out.println(str);
-
+                System.out.flush();
+                
                 String[] request = str.split(" "); // GET /index.html HTTP/1.1
                 String method = request[0];
                 String resource = request[1];
@@ -47,14 +49,14 @@ public class WebServer {
                     str = in.readLine();
                 }
 
-                switch(method){
-                    case "GET" :
+                switch (method) {
+                    case "GET":
                         handleGETRequest(remote, out, resource, httpVersion);
                         break;
-                    case "DELETE" :
+                    case "DELETE":
                         handleDELETERequest(remote, out, resource, httpVersion);
                         break;
-                    case "PUT" :
+                    case "PUT":
                         handlePUTRequest(remote, out, resource, httpVersion);
                         break;
                     default:
@@ -107,13 +109,13 @@ public class WebServer {
     }
 
 
-    private void handleDELETERequest(Socket remote, PrintWriter out, String resource, String httpVersion){
+    private void handleDELETERequest(Socket remote, PrintWriter out, String resource, String httpVersion) {
         if (resource.equals("/")) resource = "index.html";
 
         if (resource.charAt(0) == '/') resource = resource.substring(1);
 
         try {
-            if(Files.deleteIfExists(Path.of(resource))){
+            if (Files.deleteIfExists(Path.of(resource))) {
                 out.println(httpVersion + " 204 No Content");
                 out.println("Server: Pierre&Nico's Handmade Web Server");
                 out.println("");
@@ -127,36 +129,46 @@ public class WebServer {
             e.printStackTrace();
         }
     }
-  
-    private void handlePUTRequest(Socket remote, PrintWriter out, String resource, String httpVersion) throws IOException {
+
+    private void handlePUTRequest(Socket remote, PrintWriter out, String resource, String httpVersion) {
         if (resource.equals("/")) resource = "index.html";
         if (resource.charAt(0) == '/') resource = resource.substring(1);
 
-        String filetype;
-        filetype = Files.probeContentType(Path.of(resource)); // text/html or image/jpg or mp3
-        // It seems like Files.probeContentType(path) does not recognize Javascript files correctly
-        if (filetype == null && resource.split("\\.")[1].equals("js")) {
-            filetype = "text/javascript";
+        String responseCode, responseBody;
+        if(Files.exists(Path.of(resource))){
+            responseCode = "200 OK";
+            responseBody = "File replaced";
+        } else {
+            responseCode = "201 Created";
+            responseBody = "File created";
         }
 
-        if (filetype.split("/")[0].equals("text")) {
-            // text
-            out.println(httpVersion + " 200 OK");
-            out.println("Content-Type: " + filetype);
+        try {
+            //Files.copy(remote.getInputStream(), Path.of(resource), StandardCopyOption.REPLACE_EXISTING);
+
+            //FileOutputStream fileOutputStream = new FileOutputStream(resource);
+            //remote.getInputStream().transferTo(fileOutputStream);
+
+            FileWriter fileWriter = new FileWriter(resource); // If file is not found, FileNotFoundException is thrown and caught
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(remote.getInputStream()));
+            char[] line = new char[256];
+            int nbRead;
+            while ((nbRead = bufferedReader.read(line)) <= 0) { // Reading the file line per line and sending each line to the client
+                System.out.println(nbRead);
+                System.out.println(line);
+                fileWriter.write(line);
+            }
+
+            System.out.println(responseCode);
+            out.println(httpVersion + " " + responseCode);
             out.println("Server: Pierre&Nico's Handmade Web Server");
             out.println("");
-
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(remote.getInputStream()));
-
-            FileWriter fileWriter = new FileWriter(resource);
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-
-            String line;
-            while ((line = bufferedReader.readLine()) != null) { // Reading the file line per line and sending each line to the client
-                bufferedWriter.write(line);
-            }
-        } else {
-            // other cases : image, video, ...
+            out.println("<h1>" + responseBody + "</h1>");
+        } catch (IOException e) {
+            out.println(httpVersion + " 500 Internal Server Error");
+            out.println("Server: Pierre&Nico's Handmade Web Server");
+            out.println("");
+            out.println("<h1>Internal Server Error</h1>");
         }
     }
 
